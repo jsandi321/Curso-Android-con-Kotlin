fe218182924c26085435bb35d34a2bcd694b3ef7
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-07 15:53:18 -0400

    Video 8 TabletLayout

---
+++ MyApplication/app/src/main/java/com/example/pablo384/myapplication/Table_layout.kt
@@ -0,0 +1,12 @@
+package com.example.pablo384.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Table_layout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_layout)
+    }
+}
